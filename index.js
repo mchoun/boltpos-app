c50330d4887c284a6b38315261679cde2f0fed09
Mervin Choun <m.choun@gmail.com>
2021-10-08 01:16:24 -0400

    Introducting javascript

---
+++ index.js
@@ -0,0 +1,16 @@
+let mid = null;
+let hsn = null;
+let amount = 0;
+
+console.log("test")
+
+function authCard() {
+    mid = document.getElementById('mid').value;
+    hsn = document.getElementById('hsn').value;
+    amount = document.getElementById('amount').value;
+
+    console.log(mid);
+    console.log(hsn);
+    console.log(amount);
+    
+}
